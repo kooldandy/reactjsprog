ff368217d4738441704bebfb9d9d8166158706ae
Saurabh Ghosh <kkooldandy@gmail.com>
2021-09-20 16:27:56 +0530

    Seperated the webpack cofigs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-export class App extends Component<{}, {}, any>{
-  
-  public render(){
-    return(
+export class App extends Component<{}, {}, any> {
+  public render() {
+    return (
       <div>
         <h1>React App</h1>
       </div>
     )
   }
-  
 }
