f075ff93e7cc54748b2d9230751e2e7b6991de42
Saurabh Ghosh <kkooldandy@gmail.com>
2021-09-20 15:30:41 +0530

    Webpack added

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class App extends Component<{}, {}, any>{
+  
+  public render(){
+    return(
+      <div>
+        <h1>React App</h1>
+      </div>
+    )
+  }
+  
+}
